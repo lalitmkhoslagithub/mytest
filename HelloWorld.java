0da7fdb0c76588c348e0370576332f483bd65e75
Lalit Mohan Khosla <lalit.khosla@irissoftware.com>
2017-12-29 11:53:46 +0530

    My first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World. Whats up!"); // Display the string.
     }
 }
